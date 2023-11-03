9e5d3290db08dc1394c48aa53f3e5f825b10fccb
Douglas Vieira <douglas16722@gmail.com>
2023-11-03 10:41:57 -0300

    Implement single user chat
    Added JS functionality to dynamically append messages to the chat container, ensured autoscroll on new message and enhanced CSS. Also, added font-awesome to project.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,48 @@
-console.log('real-time-chat app is running!');
+(function () {
+    const isValidMessage = (message) => {
+        return message.trim() !== "";
+    };
+
+    const createMessageElement = (message) => {
+        const messageElement = document.createElement("li");
+
+        messageElement.classList.add("list-group-item", "message");
+        messageElement.innerText = message;
+
+        return messageElement;
+    };
+
+    const attachMessageToChat = (message) => {
+        const messageList = document.getElementById("messages");
+        const messageElement = createMessageElement(message);
+
+        messageList.appendChild(messageElement);
+        messageElement.scrollIntoView({behavior: "smooth"});
+    };
+
+    const sendMessage = () => {
+        const message = document.getElementById("input-msg").value;
+
+        if (!isValidMessage(message)) {
+            return;
+        }
+
+        attachMessageToChat(message);
+        document.getElementById("input-msg").value = "";
+    };
+
+    const setupEventListeners = () => {
+        const sendMessageButton = document.getElementById("send-msg-btn");
+        sendMessageButton.addEventListener("click", sendMessage);
+
+        const textArea = document.getElementById("input-msg");
+        textArea.addEventListener("keyup", (event) => {
+            if (event.key === "Enter" && !event.shiftKey) {
+                sendMessage();
+                event.preventDefault();
+            }
+        });
+    };
+
+    setupEventListeners();
+})();
